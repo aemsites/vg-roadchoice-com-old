33e24f88e598e6b10824b37c4cf23c9f5804d748
Jonatan Lledo <jonatan.lledo@netcentric.biz>
2023-07-26 17:27:01 +0200

    #11 add hotjar

--- scripts/delayed.js
+++ scripts/delayed.js
@@ -5,3 +5,13 @@ import { sampleRUM } from './lib-franklin.js';
 sampleRUM('cwv');
 
 // add more delayed functionality here
+
+// Hotjar
+/* eslint-disable */
+(function(h,o,t,j,a,r){
+  h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
+  h._hjSettings={hjid:714824,hjsv:6}; a=o.getElementsByTagName('head')[0];
+  r=o.createElement('script');r.async=1; r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
+  a.appendChild(r);
+})(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
+/* eslint-enable */
