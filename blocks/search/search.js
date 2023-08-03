f95934f97c88a36dc2fb04cd72879eb735847050
Jonatan Lledo <jonatan.lledo@netcentric.biz>
2023-08-03 18:57:11 +0200

    25 v0.2 filters are dynamically filled

--- blocks/search/search.js
+++ blocks/search/search.js
@@ -1,7 +1,9 @@
 import { createElement } from '../../scripts/scripts.js';
 
 let isCrossRefActive = true;
+const modelsItems = [];
 
+// TODO add the placeholder to the placeholders.json
 const PLACEHOLDERS = {
   crossReference: 'Cross-Reference No',
   partNumber: 'Part No',
@@ -26,16 +28,19 @@ const TEMPLATES = {
     <div class="search__make-filter__wrapper">
       <label class="search__make-filter__label">Make</label>
       <select class="search__make-filter__select shadow">
-        <option value="">Make (All)</option>
+        <option value="null">Make (All)</option>
       </select>
     </div>
     <div class="search__model-filter__wrapper">
       <label class="search__model-filter__label">Model</label>
-      <select class="search__model-filter__select shadow">
-        <option value="">model (All)</option>
+      <select class="search__model-filter__select shadow" disabled>
+        <option value="null">model (All)</option>
       </select>
     </div>
   </div>
+  `,
+  filtersResetOpt: `
+    <option value="null">model (All)</option>
   `,
   inputCR: `
   <div class="search__input-cr__container">
@@ -63,6 +68,18 @@ const TEMPLATES = {
   `,
 };
 
+function resetModelsFilter(models, disabled = true) {
+  models.innerHTML = TEMPLATES.filtersResetOpt;
+  models.disabled = disabled;
+}
+
+async function getFiltersData() {
+  const filtersUrl = '/drafts/jlledo/search-filters.json'; // TODO update the url with its final position
+  const response = await fetch(filtersUrl);
+  const json = await response.json();
+  return json;
+}
+
 function addSearchByListeners(wrapper, form) {
   wrapper.onclick = (e) => {
     if (e.target.classList.contains('active')) return;
@@ -76,6 +93,51 @@ function addSearchByListeners(wrapper, form) {
     form.querySelector('.search__input-cr__container').classList.toggle('hide', !isCrossRefActive);
     // remove the value from the not active input
     form.querySelector(`.search__input-${isCrossRefActive ? 'pn' : 'cr'}__input`).value = '';
+    if (isCrossRefActive) {
+      form.querySelector('.search__make-filter__select').selectedIndex = 0;
+      resetModelsFilter(form.querySelector('.search__model-filter__select'));
+    }
+  };
+}
+
+function populateFilter(select, items) {
+  const docRange = document.createRange();
+  let htmlFragment = '';
+  items.forEach((item) => {
+    htmlFragment += `
+      <option value="${item !== 'Others' ? item.toLowerCase() : 'null'}">${item}</option>
+    `;
+  });
+  const fragment = docRange.createContextualFragment(htmlFragment);
+  select.appendChild(fragment);
+}
+
+async function getAndApplyFiltersData(form) {
+  const makeSelect = form.querySelector('.search__make-filter__select');
+  const modelsSelect = form.querySelector('.search__model-filter__select');
+  const makeItems = [];
+  const filters = await getFiltersData();
+  const { data } = filters;
+  if (!data) return;
+  data.forEach((item) => {
+    const itemModels = item.Models !== 'null' ? item.Models.split(',') : [];
+    modelsItems.push({ Make: item.Make, Models: itemModels });
+    makeItems.push(item.Make);
+  });
+  populateFilter(makeSelect, makeItems);
+  makeSelect.onchange = (e) => {
+    const isNotNull = e.target.value !== 'null';
+    // if is null then disable the models filter
+    if (!isNotNull) {
+      resetModelsFilter(modelsSelect);
+      return;
+    }
+    // if is not null the enable the select and then is filled by the maker value
+    const models = modelsItems.filter(
+      (item) => item.Make.toLowerCase() === e.target.value,
+    )[0].Models;
+    resetModelsFilter(modelsSelect, false);
+    populateFilter(modelsSelect, models);
   };
 }
 
@@ -87,10 +149,8 @@ export default function decorate(block) {
   // Part number input and its filters are hidden by default
   form.innerHTML += TEMPLATES.filters;
   form.innerHTML += TEMPLATES.inputPN;
-
-  // add searchBy listeners
   addSearchByListeners(form.querySelector('.search__buttons__wrapper'), form);
-
+  getAndApplyFiltersData(form);
   // insert templates to form
   formWrapper.appendChild(form);
   block.appendChild(formWrapper);
