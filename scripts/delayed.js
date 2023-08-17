f856ae6364c41ab953f2387b2cb8652eec995c11
SantiagoHomps-NC <santiago.homps@netcentric.biz>
2023-08-17 12:07:51 +0200

    hotjar test

--- scripts/delayed.js
+++ scripts/delayed.js
@@ -30,7 +30,7 @@ async function loadGoogleTagManager() {
 
 // Hotjar
 /* eslint-disable */
-(function(h,o,t,j,a,r){
+(async function(h,o,t,j,a,r){
   h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
   h._hjSettings={hjid:714824,hjsv:6}; a=o.getElementsByTagName('head')[0];
   r=o.createElement('script');r.async=1; r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
