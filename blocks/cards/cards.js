99d8f161a02c2c2e3208ce458cfdb69534b93cf5
sujata kendre <sujata.kendre@netcentric.biz>
2023-08-25 15:47:23 +0530

    js and css changes

--- blocks/cards/cards.js
+++ blocks/cards/cards.js
@@ -1,18 +1,24 @@
-import { createOptimizedPicture } from '../../scripts/lib-franklin.js';
+import { createOptimizedPicture, readBlockConfig } from '../../scripts/lib-franklin.js';
+import { div } from '../../scripts/scripts.js';
 
 export default function decorate(block) {
-  /* change to ul, li */
   const ul = document.createElement('ul');
+  ul.className += 'ul_list';
   [...block.children].forEach((row) => {
     const li = document.createElement('li');
-    li.innerHTML = row.innerHTML;
+    li.className += 'li_list';
+    li.innerHTML = row.innerHTML;  
+
     [...li.children].forEach((div) => {
-      if (div.children.length === 1 && div.querySelector('picture')) div.className = 'cards-card-image';
-      else div.className = 'cards-card-body';
+      if (div.children.length === 1)
+{
+  const div1 = document.createElement('a');
+  div1.appendChild(div);
+
+}      else div.className = 'cards-card-body';
     });
     ul.append(li);
   });
-  ul.querySelectorAll('img').forEach((img) => img.closest('picture').replaceWith(createOptimizedPicture(img.src, img.alt, false, [{ width: '750' }])));
   block.textContent = '';
   block.append(ul);
 }
