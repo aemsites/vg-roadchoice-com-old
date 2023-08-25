9982c17569d39c77c2c13eb86a1a87f374843ffa
Likhita Mate <likhita.mate@netcentric.biz>
2023-08-25 19:16:36 +0530

    Fixed anchor tag

--- blocks/cards/cards.js
+++ blocks/cards/cards.js
@@ -1,23 +1,18 @@
-import { createOptimizedPicture, readBlockConfig } from '../../scripts/lib-franklin.js';
-import { div } from '../../scripts/scripts.js';
-
 export default function decorate(block) {
   const ul = document.createElement('ul');
   ul.className += 'ul_list';
   [...block.children].forEach((row) => {
     const li = document.createElement('li');
     li.className += 'li_list';
-    li.innerHTML = row.innerHTML;  
+    const div1 = document.createElement('a');
+    div1.innerHTML = row.innerHTML;
 
-    [...li.children].forEach((div) => {
-      if (div.children.length === 1)
-{
-  const div1 = document.createElement('a');
-  div1.appendChild(div);
-
-}      else div.className = 'cards-card-body';
+    [...li.children].forEach((divw) => {
+      if (divw.children.length === 1);
+      else divw.className = 'cards-card-body';
     });
     ul.append(li);
+    li.appendChild(div1);
   });
   block.textContent = '';
   block.append(ul);
