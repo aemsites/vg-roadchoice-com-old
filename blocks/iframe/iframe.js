98fd322b744711042a8291a53f521f9d09c3c0bd
Manuel Vara <manuel.vara@netcentric.biz>
2023-08-31 09:55:08 +0200

    Fix some more linting issue

--- blocks/iframe/iframe.js
+++ blocks/iframe/iframe.js
@@ -1,8 +1,7 @@
 /* eslint-disable no-use-before-define */
 import { createElement } from '../../scripts/scripts.js';
 
-export default async function decorate(block){
-
+export default async function decorate(block) {
   const link = block.querySelector('a')?.getAttribute('href');
   const iframe = createElement('iframe', { props: { src: link, loading: 'lazy', frameborder: 0 } });
   const fixedHeightClass = [...block.classList].find((el) => /[0-9]+px/.test(el));
