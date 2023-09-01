13d873f4eae623a3e19fc39e968ac03b3a68273b
sujata kendre <sujata.kendre@netcentric.biz>
2023-09-01 16:58:30 +0530

    css changes

--- blocks/cards/cards.js
+++ blocks/cards/cards.js
@@ -1,18 +1,14 @@
-export default function decorate(block) {
+export default function decorate(block) { 
   const ul = document.createElement('ul');
   ul.className += 'ul_list';
   [...block.children].forEach((row) => {
     const li = document.createElement('li');
-    li.className += 'li_list';
-    const div1 = document.createElement('a');
-    div1.innerHTML = row.innerHTML;
-
-    [...li.children].forEach((divw) => {
-      if (divw.children.length === 1);
-      else divw.className = 'cards-card-body';
-    });
-    ul.append(li);
-    li.appendChild(div1);
+    li.className += 'li_list'; 
+    const divOne = document.createElement('a');
+    divOne.innerHTML = row.innerHTML;
+    divOne.className = 'cards-card-body';
+   ul.append(li);
+    li.appendChild(divOne);
   });
   block.textContent = '';
   block.append(ul);
