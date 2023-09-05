9c74d3bd844fd30888b7df817e84dd1adb80d9a4
SantiagoHomps-NC <santiago.homps@netcentric.biz>
2023-09-05 15:32:58 +0200

    cards block and variation done

--- blocks/cards/cards.js
+++ blocks/cards/cards.js
@@ -1,15 +1,77 @@
-export default function decorate(block) { 
-  const ul = document.createElement('ul');
-  ul.className += 'ul_list';
+import { createElement } from '../../scripts/scripts.js';
+
+const blockName = 'cards';
+
+const buildCards = (block) => {
+  const ul = createElement('ul', { classes: `${blockName}-list` });
+
   [...block.children].forEach((row) => {
-    const li = document.createElement('li');
-    li.className += 'li_list'; 
-    const divOne = document.createElement('a');
-    divOne.innerHTML = row.innerHTML;
-    divOne.className = 'cards-card-body';
-   ul.append(li);
-    li.appendChild(divOne);
+    const item = createElement('li', { classes: `${blockName}-item` });
+    const cardContent = createElement('div', { classes: `${blockName}-content` });
+    const ctas = createElement('div', { classes: `${blockName}-buttons` });
+
+    const image = row.querySelector('picture');
+
+    const headings = [...row.querySelectorAll('h1, h2, h3, h4, h5, h6')];
+    headings.forEach((heading) => heading.classList.add(`${blockName}-heading`));
+
+    const buttons = [...row.querySelectorAll('a')];
+    buttons.forEach((btn) => btn.classList.add(`${blockName}-button`));
+
+    const textElmts = [];
+    row.querySelectorAll('p').forEach((p) => {
+      const hasButton = [...p.classList].includes('button-container');
+      const isEmpty = (p.textContent.trim().length === 0);
+
+      if (!isEmpty && !hasButton) textElmts.push(p);
+    });
+    textElmts.forEach((e) => e.classList.add(`${blockName}-text`));
+
+    if (image) {
+      image.classList.add(`${blockName}-image`);
+      cardContent.appendChild(image);
+    }
+    cardContent.append(...headings, ...textElmts);
+    ctas.append(...buttons);
+
+    item.append(cardContent, ctas);
+
+    ul.append(item);
   });
+
   block.textContent = '';
   block.append(ul);
+};
+
+const buildResourcesCards = (block) => {
+  const ul = createElement('ul', { classes: 'cards-list' });
+
+  [...block.children].forEach((row) => {
+    const image = row.querySelector('picture');
+    const aElmt = row.querySelector('a');
+
+    const item = createElement('li', { classes: `${blockName}-item` });
+    const card = createElement('a', { classes: `${blockName}-card`, props: { href: aElmt.href } });
+    const text = createElement('p', { classes: `${blockName}-text`, textContent: aElmt.textContent });
+
+    image.classList.add(`${blockName}-image`);
+    card.append(image, text);
+
+    item.appendChild(card);
+    ul.append(item);
+  });
+
+  block.textContent = '';
+  block.append(ul);
+};
+
+export default function decorate(block) {
+  const variant = [...block.classList];
+
+  // This detects what block needs to be render and calls the corresponding function.
+  if (variant.includes('resources')) {
+    buildResourcesCards(block);
+  } else {
+    buildCards(block);
+  }
 }
