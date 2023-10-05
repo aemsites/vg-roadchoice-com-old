12bc6818390ff9b93373c102929adc18ae8c3c9b
Jonatan Lledo <jonatan.lledo@netcentric.biz>
2023-10-05 09:27:06 +0200

    107 manuals table mobile

--- blocks/manuals-table/manuals-table.js
+++ blocks/manuals-table/manuals-table.js
@@ -2,11 +2,17 @@ export default function decorate(block) {
   const tableRows = block.querySelectorAll(':scope > div');
   [...tableRows].forEach((row) => {
     const tableColumns = row.querySelectorAll(':scope > div');
+    const oneChild = tableColumns.length === 1;
     [...tableColumns].forEach((column) => {
       const link = column.querySelector(':scope > a');
       column.removeAttribute('data-valign');
       column.className = 'manuals-table-column';
-      if (!link) return;
+      if (oneChild) column.parentElement.classList.add('one-child');
+      if (!link) {
+        column.previousElementSibling.classList.add('next-empty');
+        column.classList.add('empty');
+        return;
+      }
       column.textContent = '';
       column.appendChild(link);
     });
