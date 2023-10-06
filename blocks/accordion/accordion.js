a9d67f0a3d815718ff0727502f8df7be7ed7f29a
SantiagoHomps-NC <santiago.homps@netcentric.biz>
2023-10-06 18:13:37 +0200

    block done

---
+++ blocks/accordion/accordion.js
@@ -0,0 +1,37 @@
+import { createElement } from '../../scripts/scripts.js';
+
+const toggleAnswer = (e) => {
+  const btn = e.target;
+  const answer = btn.nextElementSibling;
+  if (answer.classList.contains('hide')) {
+    answer.classList.remove('hide');
+  } else {
+    answer.classList.add('hide');
+  }
+};
+
+export default async function decorate(block) {
+  const rows = [...block.querySelectorAll(':scope > div > div')];
+  const questionList = createElement('ul', { classes: 'accordion-question-list' });
+
+  rows.forEach((row) => {
+    const allTexts = [...row.querySelectorAll('p')];
+    const questionText = allTexts.shift();
+    const item = createElement('li', { classes: 'accordion-item' });
+    const button = createElement('button', {
+      classes: ['accordion-button'],
+      props: {
+        type: 'button',
+      },
+      textContent: questionText.innerText,
+    });
+    button.onclick = (e) => toggleAnswer(e);
+    const answer = createElement('div', { classes: ['accordion-answer', 'hide'] });
+    answer.append(...allTexts);
+    item.append(button, answer);
+    questionList.append(item);
+  });
+
+  block.textContent = '';
+  block.append(questionList);
+}
