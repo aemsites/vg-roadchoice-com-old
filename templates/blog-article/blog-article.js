96c160c5524fc9ef6634248290d41233a0ace317
SantiagoHomps-NC <santiago.homps@netcentric.biz>
2023-10-10 14:48:07 +0200

    blog refactored

--- templates/blog-article/blog-article.js
+++ templates/blog-article/blog-article.js
@@ -6,32 +6,11 @@ import { createElement } from '../../scripts/scripts.js';
 const title = getMetadata('og:title');
 const date = getMetadata('publication-date');
 
-async function buildSection(container, sectionName = '') {
-  const selectedContent = container.querySelector(`.${sectionName}-container .${sectionName}-wrapper`);
-  const sectionClassList = sectionName === 'breadcrumbs' ? ['section', 'template', 'article-template', `${sectionName}-container`] : `${sectionName}-container`;
-  const sectionContainer = createElement('div', { classes: sectionClassList });
-
-  sectionContainer.append(selectedContent);
-
-  return sectionContainer;
-}
-
 export default async function decorate(doc) {
   const container = doc.querySelector('main');
-  const article = createElement('div', { classes: ['article-content'] });
 
-  const articleTexts = createElement('div', { classes: ['section', 'template', 'article-template', 'article-texts-container'] });
-  const currentArticle = createElement('div', { classes: ['current-article-container'] });
-
-  const [
-    breadSection,
-    recommendationsSection,
-  ] = await Promise.all([
-    buildSection(container, 'breadcrumb'),
-    buildSection(container, 'recommendations'),
-  ]);
-
-  const defaultContent = container.querySelector('.default-content-wrapper');
+  const article = container.querySelector('.default-content-wrapper').parentNode;
+  article.classList.add('current-article-container');
 
   const articleTitle = createElement('h1', { classes: ['article-title'] });
   articleTitle.textContent = title;
@@ -39,13 +18,6 @@ export default async function decorate(doc) {
   const articleDate = createElement('p', { classes: ['article-date'] });
   articleDate.textContent = date;
 
-  defaultContent.insertAdjacentElement('afterbegin', articleDate);
-  defaultContent.insertAdjacentElement('afterbegin', articleTitle);
-
-  currentArticle.append(defaultContent);
-  articleTexts.append(currentArticle, recommendationsSection);
-  article.append(breadSection, articleTexts);
-
-  container.innerText = '';
-  container.append(article);
+  article.insertAdjacentElement('afterbegin', articleDate);
+  article.insertAdjacentElement('afterbegin', articleTitle);
 }
