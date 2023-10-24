392a39bec96a910b21bb2ca1750d3559a3b13560
Manuel Vara <manuel.vara@netcentric.biz>
2023-10-24 18:49:18 +0200

    Update to use GA4 tracking code

--- scripts/delayed.js
+++ scripts/delayed.js
@@ -41,16 +41,18 @@ if (!window.location.host.includes('hlx.page') && !window.location.host.includes
 
 // Google Analytics
 async function loadGoogleTagManager() {
-  // google tag manager
+  // google tag manager (gtag.js)
   // eslint-disable-next-line func-names
   (function (w, d, s, l, i) {
-    w[l] = w[l] || []; w[l].push({ 'gtm.start': new Date().getTime(), event: 'gtm.js' });
+    w[l] = w[l] || [];
+    w[l].push({ js: new Date().getTime() });
+    w[l].push({ config: `${i}` });
     const f = d.getElementsByTagName(s)[0];
     const j = d.createElement(s);
     const dl = l !== 'dataLayer' ? `&l=${l}` : ''; j.async = true;
-    j.src = `https://www.googletagmanager.com/gtm.js?id=${i}${dl}`;
+    j.src = `https://www.googletagmanager.com/gtag/js?id=${i}${dl}`;
     f.parentNode.insertBefore(j, f);
-  }(window, document, 'script', 'dataLayer', 'GTM-MJJCNZK'));
+  }(window, document, 'script', 'dataLayer', 'G-DLBC08V7CP'));
 }
 
 async function loadHotjar() {
