47538f7a8e49c29de803671d9331ca123c673286
Jonatan Lledo <jonatan.lledo@netcentric.biz>
2023-11-06 14:52:19 +0100

    space search value now show a no results page

--- blocks/search/search.js
+++ blocks/search/search.js
@@ -146,7 +146,7 @@ async function getAndApplyFiltersData(form) {
 export function searchCRPartNumValue(value, data = crData) {
   const partNumberBrands = ['OEM_num'];
   const results = new Set();
-  if (value === ''.trim()) return [];
+  if (value.trim() === '') return [];
   partNumberBrands.forEach((brand) => {
     const tempResults = data.filter(
       (item) => new RegExp(`.*${value.trim()}.*`, 'i').test(item[brand]),
@@ -206,7 +206,7 @@ export function searchPartNumValue(value, make, model, data = pnData) {
   // search by part number
   const partNumberBrands = ['Base Part Number', 'Volvo Part Number', 'Mack Part Number'];
   const results = new Set();
-  if (value === ''.trim() && make === 'null' && model === 'null') return [];
+  if (value.trim() === '' && make === 'null' && model === 'null') return [];
   partNumberBrands.forEach((brand) => {
     filterPNByColumn({ column: brand, data, value, make, model, results });
   });
